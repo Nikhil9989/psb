b7be012af1eaaa01595c8566fb8987b95531125c
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-07 12:14:02 +0530

    Fix redirection issue by removing any basePath configuration

--- next.config.js
+++ next.config.js
@@ -7,7 +7,12 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
-  }
+  },
+  // Ensure no basePath is set
+  basePath: '',
+  trailingSlash: false,
+  // Explicitly set output to be standard Next.js build
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
