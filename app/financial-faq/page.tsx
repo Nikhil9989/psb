0a57e0111f2d1d60f18a37203ad659e6e9e4d936
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-09 17:56:05 +0530

    Add Financial FAQ page

---
+++ app/financial-faq/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '@/components/layout/PageLayout';
+import FinancialFAQ from '@/components/financial/FinancialFAQ';
+
+export default function FinancialFAQPage() {
+  return (
+    <PageLayout>
+      <FinancialFAQ />
+    </PageLayout>
+  );
+}
