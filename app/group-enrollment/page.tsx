550a13f98aaf7218028494ea082b0838412e7a06
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-09 17:54:01 +0530

    Add Group Enrollment page

---
+++ app/group-enrollment/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '@/components/layout/PageLayout';
+import GroupEnrollment from '@/components/financial/GroupEnrollment';
+
+export default function GroupEnrollmentPage() {
+  return (
+    <PageLayout>
+      <GroupEnrollment />
+    </PageLayout>
+  );
+}
