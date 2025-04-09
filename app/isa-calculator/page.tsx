d4746aac0d0051071c139aed617e2bb1e9fc17cd
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-09 17:51:47 +0530

    Add ISA Calculator page

---
+++ app/isa-calculator/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageLayout from '@/components/layout/PageLayout';
+import ISACalculator from '@/components/financial/ISACalculator';
+
+export default function ISACalculatorPage() {
+  return (
+    <PageLayout>
+      <ISACalculator />
+    </PageLayout>
+  );
+}
