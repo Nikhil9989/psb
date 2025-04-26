41341b5b271656893138ba5c58386d1fadf8eb03
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-26 16:54:15 +0530

    Add missing X icon import from lucide-react

--- components/resume-builder/ResumeForm.tsx
+++ components/resume-builder/ResumeForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Plus, Minus, Trash2 } from 'lucide-react';
+import { Plus, Minus, Trash2, X } from 'lucide-react';
 import { ResumeData } from './ResumeBuilder';
 
 interface ResumeFormProps {
