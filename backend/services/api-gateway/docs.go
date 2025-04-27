8d8a186684b7dafed7425cde0c3fcf55562d99d3
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-04-27 09:12:56 +0530

    Add docs.go file for Swagger generation in API Gateway

---
+++ backend/services/api-gateway/docs.go
@@ -0,0 +1,27 @@
+// Package main SKILL BRIDGE API Gateway
+//
+// Documentation for API Gateway
+//
+//	Schemes: http, https
+//	BasePath: /api/v1
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+//	SecurityDefinitions:
+//	  BearerAuth:
+//	    type: apiKey
+//	    name: Authorization
+//	    in: header
+//
+// swagger:meta
+package main
+
+import (
+	// Import backend package for Swagger models
+	_ "github.com/Nikhil9989/psb/backend"
+)
